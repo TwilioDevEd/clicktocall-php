3ee74781baab92bff4ca440503a95ca50a2e5827
Carlos Mafla <gigo6000@hotmail.com>
2021-12-02 16:22:51 -0500

    Fix bug in outbound.php dial()

--- src/outbound.php
+++ src/outbound.php
@@ -3,20 +3,22 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Twilio\TwiML\VoiceResponse;
 
-function generate_twiml_response() {
-  $response = new VoiceResponse();
-  $response->say('Thanks for contacting our sales department. Our next available representative will take your call');
-  return (string)$response;
+function generate_twiml_response()
+{
+    $response = new VoiceResponse();
+    $response->say('Thanks for contacting PropertyLark. Our next available representative will take your call');
+
+    if (array_key_exists('QUERY_STRING', $_SERVER)) {
+        $queryArgs = array();
+        parse_str($_SERVER['QUERY_STRING'], $queryArgs);
+        $salesPhone = $queryArgs['sales_phone'];
+        // Uncomment this code and replace the number with the number you want
+        // your customers to call.
+        $response->dial($salesPhone);
+    }
+
+    return (string)$response;
 }
 
-if (array_key_exists('QUERY_STRING', $_SERVER)) {
-    $queryArgs = array();
-    parse_str($_SERVER['QUERY_STRING'], $queryArgs);
-    $salesPhone = $queryArgs['sales_phone'];
-    // Uncomment this code and replace the number with the number you want
-    // your customers to call.
-    //$response->dial($salesPhone);
-}
 
 print_r(generate_twiml_response());
-?>
